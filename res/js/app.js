8a17145e26e104f316424ea0742a83af4957fc79
luijar <luijar@luisatencio.net>
2016-01-10 11:35:33 -0500

    New Template

--- res/js/app.js
+++
@@ -1,8 +0,0 @@
-require(['tuple'], function(Tuple) {
-	
-	var BIG_DAY = new Date(2016, 12-6, 1);
-	
-	$(document).ready(function () {					
-		$('#defaultCountdown').countdown({until: $.countdown.UTCDate(-4, BIG_DAY)});		
-	});	
-});
